19997c98e739488e651c9346b5373d1308956c6c
Koutoubu <bienvenne.3141@au.com>
2021-09-28 01:47:07 +0900

    ユーザー管理機能とRSpecの導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_KANA, presence: true
+  validates :first_name_KANA, presence: true
+  validates :birthday, presence: true
+  validates :encrypted_password,:password,:password_confirmation,length:{minimum:5},format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/}
+
+
+  has_many :items
+  has_many :shares
 end
