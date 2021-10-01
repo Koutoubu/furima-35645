67ea533b2495ab67350a27a2012fdcf2d10ec153
Koutoubu <bienvenne.3141@au.com>
2021-10-01 12:46:42 +0900

    テストコード

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,6 @@
 FactoryBot.define do
   factory :user do
     nickname              {'test'}
-    email                 {'test@example'}
-    password              {'000000'}
-    password_confirmation {password}
     first_name            {'太郎'}
     last_name             {'佐藤'}
     first_name_KANA       {'タロウ'}
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,24 +10,6 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors.full_messages).to include "Nickname can't be blank"
     end
-
-    it 'emailが空では登録できない' do
-      @user.email = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include "Email can't be blank"
-    end
-
-    it 'passwordが空では登録できない' do
-      @user.password = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Password can't be blank")
-    end
-
-    it 'passwordとpassword_confirmationが不一致では登録できない' do
-      @user.password_confirmation = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
-    end
     it 'first_nameが空では登録できない' do
       @user.first_name = ''
       @user.valid?
