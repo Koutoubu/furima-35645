f6b20efbeaf31f8e607ae16b3e59c9f0d9157861
Koutoubu <bienvenne.3141@au.com>
2021-10-04 08:46:24 +0900

    Revert "ミス"
    This reverts commit 2eed69face0d0ce2f2cff17459181b7f14e0d6b0.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create] do
-    resources :messages, only: [:index, :create]
-  end
 end
--- db/migrate/20211003152613_create_items.rb
+++ db/migrate/20211003152613_create_items.rb
@@ -1,8 +1,7 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.string  :content
-      t.references :user, null: false, foreign_key: true
+
       t.timestamps
     end
   end
