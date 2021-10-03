2eed69face0d0ce2f2cff17459181b7f14e0d6b0
Koutoubu <bienvenne.3141@au.com>
2021-10-04 08:46:10 +0900

    ミス

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
+  resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 end
