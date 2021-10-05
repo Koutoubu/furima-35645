6c69e24a67b8233f69cc2693acea34dc6824b8f9
Koutoubu <bienvenne.3141@au.com>
2021-10-05 16:22:12 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,25 @@
 class ItemsController < ApplicationController
+
+  def index
+    @items = Item.all
+  end
+
+  def new
+    @items = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
 end
