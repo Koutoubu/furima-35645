bddd835c8bfeca7253017c96d79266e41641c14a
Koutoubu <bienvenne.3141@au.com>
2021-10-05 16:22:22 +0900

    Revert "修正"
    This reverts commit 6c69e24a67b8233f69cc2693acea34dc6824b8f9.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,25 +1,2 @@
 class ItemsController < ApplicationController
-
-  def index
-    @items = Item.all
-  end
-
-  def new
-    @items = Item.new
-  end
-
-  def create
-    @item = Item.new(item_params)
-    if @item.save
-      redirect_to root_path
-    else
-      render :new
-    end
-  end
-  private
-
-  def prototype_params
-    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
-  end
-
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,10 +4,4 @@ class Item < ApplicationRecord
   has_one    :shared
   belongs_to :user
   has_one_attached :image
-
-  validates :name, presence: true
-  validates :explanation, presence: true
-  validates :category_id, :status_id, :seller_id, :area_id, :date_id, :images, presence: true
-  validates :price, presence: true, numericality: { in: 300..9999999 }, format:{with: /\A[0-9]+\z/ }
-
 end
