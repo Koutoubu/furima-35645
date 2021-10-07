6c21fc882533c473abb8f1d3be8573877c72d603
Koutoubu <bienvenne.3141@au.com>
2021-10-07 17:29:33 +0900

    出品機能の実装　コントローラー、route

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,25 @@
 class ItemsController < ApplicationController
+
+  def index
+    @items = Item.all
+  end
+
+  def new
+    @items = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price, :explanation, :seller_id, :category_id, :status_id, :area_id, :date_id, :image).merge(user_id: current_user.id)
+  end
+
 end
