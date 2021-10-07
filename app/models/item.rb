4aad0543d32b4cbdc979da7ef7802b7e51271f43
Koutoubu <bienvenne.3141@au.com>
2021-10-07 20:31:45 +0900

    itemsのバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
-  has_one :shared
+  has_one    :shared
   belongs_to :user
+
+  validates :name, presence: true
+  validates :explanation, presence: true
+  validates :category_id, :status_id, :seller_id, :area_id, :date_id, :images, presence: true
+  validates :price, presence: true, numericality: { in: 300..9999999 }, format:{with: /\A[0-9]+\z/ }
+
 end
