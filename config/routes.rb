84c338796b4cd0290f0f11ce213143c524c58832
Koutoubu <bienvenne.3141@au.com>
2021-10-08 18:59:36 +0900

    Revert "ミス"
    This reverts commit 796129725878607e4306db83a1864987e9f88db3.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,4 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :items, only: [:new, :create]
-  
-  root to: 'articles#index'
-  resources :articles
 end
