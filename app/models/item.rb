e56be3981b2c3231b74e47cbe2b6e7056d3f71d1
Koutoubu <bienvenne.3141@au.com>
2021-10-09 13:05:20 +0900

    ActiveHash category

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,14 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   has_one    :shared
   belongs_to :user
   has_one_attached :image
+  belongs_to :category_id, :status_id, :seller_id, :date_id
 
   validates :name, presence: true
   validates :explanation, presence: true
-  validates :category_id, :status_id, :seller_id, :area_id, :date_id, :images, presence: true
+  validates :images, presence: true
+  validates :category_id, :status_id, :seller_id, :area_id, :date_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :price, presence: true, numericality: { in: 300..9999999 }, format:{with: /\A[0-9]+\z/ }
 
 end
