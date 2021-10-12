3d4d0530d6273f59249a7a3c6dd16f9c999b2d70
Koutoubu <bienvenne.3141@au.com>
2021-10-12 16:29:18 +0900

    Javaの修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,10 +1,13 @@
-window.addEventListener('load', () => {
-  const priceInput = document.getElementById("item-price");
+function itemPrice(){
+  const profit = document.getElementById('profit');
+  const addTaxDom = document.getElementById('add-tax-price');
+  const priceInput = document.getElementById('item-price');
+
   priceInput.addEventListener("input", () => {
-    const inputValue = priceInput.value;
-    const addTaxDom = document.getElementById("add-tax-price");
-    addTaxDom.innerHTML = inputValue * 0.1
-    const profit = document.getElementById("profit");
-    profit.innerHTML = inputValue - addTaxDom.innerHTML
-  })
-});
+    const inputValue = priceInput.value
+    const salesCommission = Math.floor(inputValue * 0.1)
+    addTaxDom.innerHTML = salesCommission
+    profit.innerHTML = Math.floor(inputValue - salesCommission)
+  });
+}
+window.addEventListener('load', itemPrice);
