3d83ac3fd5c6ee2b2272c990def961e3312e301d
Koutoubu <bienvenne.3141@au.com>
2021-10-14 00:37:26 +0900

    ミス

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_11_104329) do
+ActiveRecord::Schema.define(version: 2021_10_13_041252) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -48,9 +48,22 @@ ActiveRecord::Schema.define(version: 2021_10_11_104329) do
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
+  create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "postal_number", null: false
+    t.integer "area_id", null: false
+    t.string "municipalities", null: false
+    t.string "address", null: false
+    t.string "building", null: false
+    t.string "tell_number", null: false
+    t.bigint "shared_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["shared_id"], name: "index_orders_on_shared_id"
+  end
+
   create_table "shareds", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "item_id"
+    t.bigint "user_id", null: false
+    t.bigint "item_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["item_id"], name: "index_shareds_on_item_id"
@@ -77,6 +90,7 @@ ActiveRecord::Schema.define(version: 2021_10_11_104329) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "items", "users"
+  add_foreign_key "orders", "shareds"
   add_foreign_key "shareds", "items"
   add_foreign_key "shareds", "users"
 end
---
+++ spec/requests/shared_request_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "Shareds", type: :request do
+
+end
